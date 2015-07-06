9af250a0fe285fb1b0805f784f7277e7c983ca6d
Sajit Thomas <sthomas@esri.com>
2015-07-06 12:39:02 -0400

    Update summary-viewer to 3.14

---
+++ js/nls/zh-tw/resources.js
@@ -0,0 +1,12 @@
+﻿define(
+    ({
+      "map": {
+         "error": "無法建立地圖"
+      },
+      "tooltips": {
+         "logo": "標誌",
+         "switchbasemap": "切換底圖",
+         "close": "關閉"
+      }
+   })
+);
